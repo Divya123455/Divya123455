523295716e9c9573d5ef346de7fe4f0104de7049
Divya123455 <100204159+Divya123455@users.noreply.github.com>
2022-02-23 18:42:56 +0530

    Add files via upload

---
+++ Exercise1.java
@@ -0,0 +1,14 @@
+// Wriite a program to print your Name, Father's Name and Mailing Address in different lines
+public class Exercise1
+{
+public static void main(String args[]) 
+{
+        System.out.println("my name is Divya");
+        System.out.println("my father father is Murthy");
+        System.out.println("my emailid:divyakasavajula@gmail.com");
+        
+}
+
+
+
+} 
