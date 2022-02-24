240fc33c7585c56a1c9adfd1a305fe4ea7db3a9a
Divya123455 <100204159+Divya123455@users.noreply.github.com>
2022-02-24 20:18:24 +0530

    Add files via upload

---
+++ Exercise2.java
@@ -0,0 +1,22 @@
+/* Write a program to find number of days in a month
+Test Data: 
+month number=2
+year=2020
+Expected Output: February 2020 has 29 days
+*/
+
+class Exercise2 {
+
+	public static void main(String args[]) {
+		int month=2, year=2020;
+		
+		if(month==2){
+		System.out.println("february 2020 has 29 days");
+		}
+		
+		
+
+
+	}
+
+}
