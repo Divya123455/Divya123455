f152a57e777a311384b6ccf0b7d03f9254d1dd81
Divya123455 <100204159+Divya123455@users.noreply.github.com>
2022-02-24 23:21:07 +0530

    Add files via upload

---
+++ PrintNum.java
@@ -0,0 +1,19 @@
+class LoopExample{
+    
+    public static void main(String args[]){
+        
+        Scanner s = new Scanner(System. in);
+        
+        System.out.println("Enter n value");
+        int n=s.nextInt();  //50
+        
+          int i=0;
+          
+          for(i=1;i<=n;i++){     // i=0 , 1 2
+              
+              System.out.print(i+" ");
+          }                                        // hi hi hi 
+    
+       
+    }
+}
