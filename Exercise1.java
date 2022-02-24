f98341ad5c2d5515c212b4d52280286e257f2ba6
Divya123455 <100204159+Divya123455@users.noreply.github.com>
2022-02-24 13:45:19 +0530

    Add files via upload

--- Exercise1.java
+++ Exercise1.java
@@ -1,14 +1,15 @@
-// Wriite a program to print your Name, Father's Name and Mailing Address in different lines
-public class Exercise1
-{
-public static void main(String args[]) 
-{
-        System.out.println("my name is Divya");
-        System.out.println("my father father is Murthy");
-        System.out.println("my emailid:divyakasavajula@gmail.com");
-        
+// Write a program to initialize a char variable and then store it in an int variable. Print the int variable's value on console.
+
+class Exercise1 {
+	public static void main(String args[]) {
+        char ch = 'a';
+        System.out.println("char value: " + ch);
+        int b = int(ch) ;
+        System.out.println("int value: " + a);
+    }
 }
+		
+	
+	
 
 
-
-} 
