065a478c73347043625844f86c77bffa2d634645
Divya123455 <100204159+Divya123455@users.noreply.github.com>
2022-03-07 12:53:40 +0530

    Add files via upload

--- Exercise2.java
+++ Exercise2.java
@@ -1,22 +1,16 @@
-/* Write a program to find number of days in a month
-Test Data: 
-month number=2
-year=2020
-Expected Output: February 2020 has 29 days
-*/
-
+//  Write a program to create a two dimensional array with duplicate elements and print only the duplicate elements.
 class Exercise2 {
 
 	public static void main(String args[]) {
-		int month=2, year=2020;
-		
-		if(month==2){
-		System.out.println("february 2020 has 29 days");
-		}
-		
-		
 
-
-	}
-
+		    int arry[][]={{1,1,1},{2,2,2},{3,3,3}};
+		    for (int i=0;i<arry.length;i++)
+		    {
+		    for (int j=0;arry.length<i;j++)
+		    {
+		        System.out.println(arry[i][j]+ " ");
+		    }
+		    System.out.println();
+   }
+}
 }
