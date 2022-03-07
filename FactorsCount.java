de8d7f27e4a5abd11831d5a8c8544cd78fe0e414
Divya123455 <100204159+Divya123455@users.noreply.github.com>
2022-03-07 12:35:14 +0530

    Add files via upload

--- FactorsCount.java
+++ FactorsCount.java
@@ -1,10 +1,4 @@
-WAP to print number of factors for the given number.
-
-
-   10  ->   1 2 5
-           number of factors is: 3
-
-
+//WAP to print number of factors for the given number.
 import java.util.Scanner;
 class LoopExample{
     
@@ -14,9 +8,9 @@ class LoopExample{
          System.out.println("Enter n value");
          int n=s.nextInt();
          int count=0;
-         for(int i=1;i<=n/2;i++){      //100   1 2 3 4....50
+         for(int i=1;i<=n/2;i++){       
              
-             if(n%i==0){           // 1 2 4 5 10 20 25 50
+             if(n%i==0){           
                  count++;
              }
          }
